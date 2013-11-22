ee2b91c1c9e16b3662202e766fe76d4baf3ebebf
Christopher Pappas <damassi.pappas@gmail.com>
2013-11-21 17:24:16 -0800

    Updated comm

---
+++ GruntFile.js
@@ -0,0 +1,69 @@
+/**
+ * Default test runner
+ *
+ * Primary Tasks:
+ *   tests  :  Execute mocha tests
+ *   dev    :  Development mode, file-watcher
+ */
+
+
+module.exports = function( grunt ) {
+
+
+	grunt.initConfig({
+
+
+    // + ---------------------------------------
+
+
+		'simplemocha': {
+			options: {
+  		  ui: 'bdd',
+  		  reporter: 'spec',
+        compilers: 'coffee:coffee-script',
+        bail: true
+  		},
+
+		  all: {
+        src: ['test/ranker_spec.coffee']
+      }
+		},
+
+
+    'watch': {
+      src: {
+        files: [ 'src/**', 'test/**' ],
+        tasks: [ 'simplemocha' ]
+      }
+    }
+
+	})
+
+
+  // + ---------------------------------------
+
+
+  grunt.registerTask( 'dev', [
+    'tests',
+    'watch'
+  ])
+
+  grunt.registerTask( 'tests', [
+    'simplemocha'
+  ])
+
+
+  // + ---------------------------------------
+
+
+  grunt.loadNpmTasks( 'grunt-contrib-watch' )
+  grunt.loadNpmTasks( 'grunt-simple-mocha' )
+
+
+  // + ---------------------------------------
+
+
+  grunt.option( 'force', true )
+
+
+}
